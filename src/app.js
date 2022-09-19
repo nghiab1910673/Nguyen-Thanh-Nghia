4786159117ff791c3a6f57a6c4ad043caf018f09
unknown <nghiab1910673>
2022-09-19 10:44:00 +0700

    Implement handlers

--- src/app.js
+++ src/app.js
@@ -12,7 +12,8 @@ app.route('/api/contacts')
     .get(contactController.findAll)
     .post(contactController.create)
     .delete(contactController.deleteAll);
-app.route('/api/contacts/favorite').get(contactController.findAllFavorite);
+app.route('/api/contacts/favorite')
+    .get(contactController.findAllFavorite);
 
 app.route('/api/contacts/:id')
     .get(contactController.findOne)
@@ -29,9 +30,8 @@ app.use((req, res, next) => {
     // Call next() to pass to the error handling middleware.
     return next(new ApiError(404, 'Resource not found'));
 });
-// Define error-handling middleware last, after other app.use() and routes
-calls.
-    app.use((err, req, res, next) => {
+// Define error-handling middleware last, after other app.use() and routes calls.
+    app.use((error, req, res) => {
         // The centralized error handling middleware.
         // In any route handler, calling next(error)
         // will pass to this error handling middleware.
